d121dbf99720eb9edc257478c36a1ef8956d4c53
bong <jungcome7@gmail.com>
2024-03-18 17:42:07 +0900

    refactor: move the user directory under the domains directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
 import { HealthCheckController } from './health-check/health-check.controller';
 import { DogHealthIndicator } from './health-check/dog.health';
-import { UserModule } from './user/user.module';
+import { UserModule } from './domains/user/user.module';
 
 @Module({
   imports: [
