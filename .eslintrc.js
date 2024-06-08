59d06adb1b425c63979d63a2987956326e78c6b2
bong <jungcome7@gmail.com>
2024-06-08 13:42:45 +0900

    fix: fix .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,17 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
+  plugins: ['@typescript-eslint'],
   root: true,
   env: {
     node: true,
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+  rules: {},
 };
