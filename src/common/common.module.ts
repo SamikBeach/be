34a2fa75876a463f010b54e89741edcf27dc5d5d
bong <jungcome7@gmail.com>
2024-10-30 22:25:23 +0900

    chore

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
+import { DefaultLogger } from './logger/default.logger';
 
 @Module({
   controllers: [CommonController],
-  providers: [CommonService],
+  providers: [CommonService, DefaultLogger],
   exports: [CommonService],
 })
 export class CommonModule {}
