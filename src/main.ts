2e5b472ca4b7dfcb88e2a936b42078684c43e982
bong <jungcome7@gmail.com>
2024-10-30 22:07:39 +0900

    feat: uri에 /api prefix 추가 & versioning(v1)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -31,6 +31,12 @@ async function bootstrap() {
   });
 
   app.use(cookieParser());
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
